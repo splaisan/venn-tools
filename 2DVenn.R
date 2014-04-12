92f626874b06420c087751e7a105d86a0011d769
Stephane Plaisance <stephane.plaisance@vib.be>
2014-04-12 13:28:55 +0200

    Update 2DVenn.R

--- 2DVenn.R
+++ 2DVenn.R
@@ -1,11 +1,7 @@
 #!/usr/bin/RScript
 
 # plots a 2D VENN from precomputed data
-<<<<<<< HEAD
-# usage: 2Dvenn.R -a A-cnt -b B-cnt -i AB-cnt -A A-label -B B-label -t "My Title" -o myVenn.png
-=======
 # usage: 2Dvenn.R -a A-cnt -b B-cnt -i AB-cnt -A A-label -B B-label -t "My Title" -o myVenn.png -U 2
->>>>>>> FETCH_HEAD
 
 # counts are epected in the order
 # "10","11","01"
